36949b3150df14748755b798b95079dbe9ddad37
Kyle-P-Bush <kylebush64@gmail.com>
2025-03-04 17:21:10 -0600

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const split = string.split("");
+    let reversed = "";
+    for(let i = split.length - 1; i >=0; i--) {
+        reversed += split[i]; 
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
